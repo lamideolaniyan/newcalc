edf2ad3ef4e6492d88fb5cd68a8eeaef50e532ef
lamideolaniyan <olaniyanolamide@gmail.com>
2023-08-17 20:03:28 +0100

    coded the bare basic logic of the calculator. It adds, multiplies, divides and substracts only 2 figures for now

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ TO DO
 const database = {};
 const getDOM = { btnArr: Array.from(document.querySelectorAll('.calc__btn')) };
 let numStr = '';
-const calcArr = [];
+let calcArr = [];
+let result = 0;
 
 let calcStr = '';
 const getUserInput = () => {
@@ -19,6 +20,14 @@ const getUserInput = () => {
 		btn.addEventListener('click', (e) => {
 			if (e.target.dataset.type === 'num') {
 				numStr += e.target.dataset.value;
+			} else if (e.target.dataset.value === '=') {
+				// DO CALCULATION
+				calcArr.push(numStr);
+				calcNum();
+				console.log(calcArr);
+
+				// RETURN RESULT
+				// STORE RESULT IN ARRAY
 			} else if (e.target.dataset.type === 'operator') {
 				calcArr.push(numStr);
 				calcArr.push(e.target.dataset.value);
@@ -29,4 +38,34 @@ const getUserInput = () => {
 	});
 };
 
+const calcNum = () => {
+	result = calcArr.reduce((acc, curr, ind) => {
+		switch (curr) {
+			case '*':
+				result = acc * 1 * calcArr[ind + 1];
+				calcArr = [];
+				numStr = '';
+				console.log(result);
+				break;
+			case '+':
+				result = acc * 1 + calcArr[ind + 1] * 1;
+				calcArr = [];
+				numStr = '';
+				console.log(result);
+				break;
+			case '-':
+				result = acc * 1 - calcArr[ind + 1] * 1;
+				calcArr = [];
+				numStr = '';
+				console.log(result);
+				break;
+			case '/':
+				result = (acc * 1) / (calcArr[ind + 1] * 1);
+				calcArr = [];
+				numStr = '';
+				console.log(result);
+				break;
+		}
+	});
+};
 getUserInput();
